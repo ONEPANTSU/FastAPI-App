8a2070369d45b804ba3c11daae368dc8ea711bc3
ONEPANTSU <conandet@mail.ru>
2023-06-21 02:51:48 +0300

    feat: pydantic

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from fastapi import FastAPI
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import ValidationError
+from fastapi.responses import JSONResponse
 from loguru import logger
+from pydantic import BaseModel, Field
 
 logger.add(
     "app.log",
@@ -11,11 +19,46 @@ logger.add(
 
 app = FastAPI(title="Trading App")
 
+
 users = [
     {"id": 1, "role": "admin", "name": "Bob"},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"},
+    {
+        "id": 2,
+        "role": "investor",
+        "name": "John",
+        "degree": [
+            {"id": 1, "created_at": "2023-06-21T02:35:00", "type_degree": "newbie"}
+        ],
+    },
+    {
+        "id": 3,
+        "role": "trader",
+        "name": "Matt",
+        "degree": [
+            {"id": 1, "created_at": "2023-05-21T02:35:00", "type_degree": "newbie"},
+            {"id": 2, "created_at": "2023-06-21T02:35:00", "type_degree": "expert"},
+        ],
+    },
 ]
+
+
+class DegreeType(Enum):
+    newbie = "newbie"
+    expert = "expert"
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[List[Degree]]
+
 
 trades = [
     {
@@ -37,24 +80,33 @@ trades = [
 ]
 
 
-@app.get("/users/{user_id}", tags=["users"])
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: float
+
+
+@app.exception_handler(ValidationError)
+async def validation_exception_handler(request: Request, exc: ValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors()}),
+    )
+
+
+@app.get("/users/{user_id}", tags=["users"], response_model=List[User])
 @logger.catch
 def get_user(user_id: int):
     logger.info(f"GET\t/users/{user_id}\t|\tparams: user_id={user_id}")
     return [user for user in users if user.get("id") == user_id]
 
 
-@app.get("/trades", tags=["trades"])
+@app.post("/trades", tags=["trades"])
 @logger.catch
-def get_trades(limit: int = 10, offset: int = 0):
-    logger.info(f"GET\t/trades\t|\tparams: limit={limit}, offset={offset}")
-    return trades[offset:][:limit]
-
-
-@app.post("/users/{user_id}", tags=["users"])
-@logger.catch
-def change_user_name(user_id: int, new_name: str):
-    logger.info(f"POST\t/users/{user_id}\t|\tparams: user_id={user_id}, new_name={new_name}")
-    current_user = list(filter(lambda user: user.get("id") == user_id, users))[0]
-    current_user["name"] = new_name
-    return {"status": 200, "data": current_user}
+def post_trades(new_trades: List[Trade]):
+    logger.info(f"POST\t/trades\t|\tparams: new_trades={new_trades}")
+    trades.extend(new_trades)
+    return {"status": 200, "data": trades}
